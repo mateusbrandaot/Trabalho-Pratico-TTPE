0350920461686bec882716c6cd432a7c09579f81
mateusbrandao <mateus.brandaoteixeira@gmail.com>
2023-11-29 15:27:09 -0300

    estoquetest add

--- src/test/java/ttpe/trabalho/test/EstoqueTest.java
+++ src/test/java/ttpe/trabalho/test/EstoqueTest.java
@@ -1,56 +1,78 @@
 package ttpe.trabalho.test;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import ttpe.trabalho.exception.DescricaoEmBrancoException;
 import ttpe.trabalho.exception.ProdutoInvalidoException;
+import ttpe.trabalho.exception.ValorInvalidoException;
+import ttpe.trabalho.model.Empresa;
 import ttpe.trabalho.model.Estoque;
+import ttpe.trabalho.model.Fornecedor;
 import ttpe.trabalho.model.Produto;
 
 class EstoqueTest {
 
+	   private String id = "1";
+	   private String nomePadrao = "produto teste";
+       private String descricaoPadrao = "Parafuso Phillips 6Mm X 41Mm";
+       private String codigoBarraPadrao = "00025163";
+       private double precoPadrao = 10.5;
+       private int qtdAtualPadrao = 5;
+       private Empresa empresaPadrao = new Empresa(1, "35737451000190", "MATRIZ", "rua portugal");
+       private Fornecedor fornecedorPadrao = new Fornecedor(1, "fornecedor test", "6198460585", "endereço teste");
+       private int qtdMinimaPadrao = 30;
+       private Date dataAtualPadrao = new Date();
+   
     private Estoque estoque;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws DescricaoEmBrancoException, ValorInvalidoException {
         estoque = new Estoque();
-        // Adicionar alguns produtos ao estoque para os testes
-       // estoque.adicionarProduto(new Produto(/* parâmetros do construtor */)); // Exemplo
-        // Continue adicionando conforme necessário
+        estoque.adicionarProduto(new Produto(id, nomePadrao, descricaoPadrao, codigoBarraPadrao, precoPadrao, qtdAtualPadrao, empresaPadrao, fornecedorPadrao, qtdMinimaPadrao, dataAtualPadrao)); // Exemplo
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"idValido", "idInvalido"}) // Adicione IDs de exemplo
-    void testGetProdutoPorId(String id) {
-        try {
+    @ValueSource(strings = {"1", "2"})
+    void testGetProdutoPorId(String id) throws ProdutoInvalidoException {
+        if (id.equals("2")) {
+            Assertions.assertThrows(ProdutoInvalidoException.class, () -> {
+                estoque.getProdutoPorId(id);
+            });
+        } else {
             Produto produto = estoque.getProdutoPorId(id);
             Assertions.assertNotNull(produto);
-        } catch (ProdutoInvalidoException e) {
-            Assertions.assertTrue(id.equals("idInvalido"));
         }
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"nomeValido", "nomeInvalido"}) // Adicione nomes de exemplo
-    void testGetProdutoPorNome(String nome) {
-        try {
-            Produto produto = estoque.getProdutoPorNome(nome);
-            Assertions.assertNotNull(produto);
-        } catch (ProdutoInvalidoException e) {
-            Assertions.assertTrue(nome.equals("nomeInvalido"));
-        }
-    }
 
     @ParameterizedTest
-    @ValueSource(strings = {"codigoBarraValido", "codigoBarraInvalido"}) // Adicione códigos de barras de exemplo
-    void testGetProdutoPorCodigoBarra(String codigoBarra) {
-        try {
-            Produto produto = estoque.getProdutoPorCodigoBarra(codigoBarra);
+    @ValueSource(strings = {"produto teste", "produto teste inexistente"})
+    void testGetProdutoPorNome(String nome) throws ProdutoInvalidoException {
+        if (nome.equals("produto teste inexistente")) {
+            Assertions.assertThrows(ProdutoInvalidoException.class, () -> {
+                estoque.getProdutoPorNome(nome);
+            });
+        } else {
+            Produto produto = estoque.getProdutoPorId(id);
             Assertions.assertNotNull(produto);
-        } catch (ProdutoInvalidoException e) {
-            Assertions.assertTrue(codigoBarra.equals("codigoBarraInvalido"));
+        }
+    }
+   
+    @ParameterizedTest
+    @ValueSource(strings = {"00025163", "00000000"})
+    void testGetProdutoPorCodigoBarra(String codigoBarra) throws ProdutoInvalidoException {
+        if (codigoBarra.equals("00000000")) {
+            Assertions.assertThrows(ProdutoInvalidoException.class, () -> {
+                estoque.getProdutoPorCodigoBarra(codigoBarra);
+            });
+        } else {
+            Produto produto = estoque.getProdutoPorId(id);
+            Assertions.assertNotNull(produto);
         }
     }
 }
